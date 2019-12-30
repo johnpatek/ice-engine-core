aa43bffe20358e915c4265f4c27a484f419f0f0a
john <jpatek@ubiqsecurity.com>
2019-12-30 14:58:54 -0800

    Added buffer, types, and json functionality

---
+++ src/include/buffer.h
@@ -0,0 +1,10 @@
+#include "types.h"
+#include <vector>
+
+namespace ice
+{
+    class buffer : public std::vector<byte_type>
+    {
+        
+    };
+}
---
+++ src/include/types.h
@@ -0,0 +1,9 @@
+#include <kj/common.h>
+#include <json11.hpp>
+namespace ice
+{
+    typedef kj::byte byte_type;
+    typedef size_t size_type;
+    typedef json11::Json Json;
+    
+}
