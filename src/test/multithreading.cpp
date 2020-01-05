292babe04d3a926101585b98c53d311506d7bb45
john <jpatek@ubiqsecurity.com>
2020-01-05 15:45:43 -0800

    Stopping point

--- src/test/multithreading.cpp
+++ src/test/multithreading.cpp
@@ -4,22 +4,22 @@
 
 ICE_TEST_CASE(threadpool_single_test)
 {
-    ice::core::thread_pool test_pool_1(1);
-    ice::core::heap_buffer test_buffer_1;
-    const int test_value_1 = 100;
+    ice::core::thread_pool test_pool(1);
+    ice::core::heap_buffer test_buffer;
+    const int test_value = 100;
 
-    auto expected_result_1 = test_value_1;
+    auto expected_result = test_value;
 
-    auto future_result = test_pool_1.enqueue(
+    auto future_result = test_pool.enqueue(
         [](int x)
         {
             return x;
         },
-        test_value_1);
+        test_value);
     
-    auto actual_result_1  = future_result.get();
+    auto actual_result  = future_result.get();
 
-    ICE_TEST_ASSERT(expected_result_1 == actual_result_1)
+    ICE_TEST_ASSERT(expected_result == actual_result)
 }
 
 ICE_TEST_CASE(threadpool_multiple_test)
@@ -30,7 +30,7 @@ ICE_TEST_CASE(threadpool_multiple_test)
 
     const unsigned int totalms = (tasks * delayms) / threads;
     
-    ice::core::thread_pool test_pool_1(threads);
+    ice::core::thread_pool test_pool(threads);
     
     std::vector<std::pair<int, std::future<int>>> test_results(tasks);
     
@@ -38,7 +38,7 @@ ICE_TEST_CASE(threadpool_multiple_test)
 
     for (auto & p : test_results) {
         p.first = rand();
-        p.second = test_pool_1.enqueue(
+        p.second = test_pool.enqueue(
             [delayms](int x)
             {
                 std::this_thread::sleep_for(
