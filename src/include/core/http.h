284d5f31bd0aaab44c926b8bdbc47c8b8532b125
john <jpatek@ubiqsecurity.com>
2020-02-13 23:57:12 -0800

    Trying to fix build process

--- src/include/core/http.h
+++ src/include/core/http.h
@@ -1,10 +1,5 @@
 #ifndef HTTP_H
 #define HTTP_H
-#include <cpprest/http_msg.h>
-#include <cpprest/http_client.h>
-#include <cpprest/http_listener.h>
-#include <core/memory.h>
-#include <core/multithreading.h>
 
 namespace ice
 {
