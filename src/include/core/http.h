b0522634d73ed988e26a14ceea6b9dc0569998b8
john <jpatek@ubiqsecurity.com>
2020-02-20 00:22:13 -0800

    Added SDL2

--- src/include/core/http.h
+++ src/include/core/http.h
@@ -1,11 +1,27 @@
 #ifndef HTTP_H
 #define HTTP_H
+#include <cpprest/http_msg.h>
+#include <cpprest/http_client.h>
+#include <cpprest/http_listener.h>
 
 namespace ice
 {
 namespace core
-{
+{   
+    typedef web::http::http_headers http_headers;
+    typedef web::http::experimental::listener::http_listener http_listener;
+    typedef web::http::http_request http_request;
+    typedef web::http::http_response http_response;
+    typedef web::http::methods http_methods;
+    typedef web::http::status_codes status_codes;
 
+    class http_threaded_server
+    {
+    private: 
+        http_listener _listener;
+
+        
+    };
 }
 }
 #endif
--- src/include/core/types.h
+++ src/include/core/types.h
@@ -1,10 +1,12 @@
 #ifndef TYPES_H
 #define TYPES_H
+#include <string>
 #include <numeric>
 namespace ice
 {
 namespace core
 {
+    typedef std::string string_type;
     typedef unsigned char byte_type;
     typedef std::size_t size_type;
 }
